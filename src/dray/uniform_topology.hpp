b8cbfe2b1a2c00a25e7c6f1f83ff423a7452683e
Matt Larsen <larsen30@llnl.gov>
2019-12-27 14:26:15 -0800

    uniform base

---
+++ src/dray/uniform_topology.hpp
@@ -0,0 +1,42 @@
+// Copyright 2019 Lawrence Livermore National Security, LLC and other
+// Devil Ray Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#ifndef DRAY_UNIFORM_TOPOLOGY_HPP
+#define DRAY_UNIFORM_TOPOLOGY_HPP
+
+#include <dray/topology_base.hpp>
+
+namespace dray
+{
+
+class UniformTopology : public TopologyBase
+{
+
+protected:
+  Vec<Float, 3> m_spacing;
+  Vec<Float, 3> m_origin;
+  Vec<int32, 3> m_dims;
+public:
+  UniformTopology() = delete;
+  UniformTopology(const Vec<Float,3> &spacing,
+                  const Vec<Float,3> &m_origin,
+                  const Vec<int32,3> &dims);
+
+  virtual ~UniformTopology();
+  virtual int32 cells() const override;
+
+  virtual int32 order() const override;
+
+  virtual int32 dims() const override;
+
+  virtual std::string type_name() const override;
+
+  virtual AABB<3> bounds() const override;
+  virtual Array<Location> locate (Array<Vec<Float, 3>> &wpoints) const override;
+};
+
+} // namespace dray
+
+#endif // DRAY_REF_POINT_HPP
